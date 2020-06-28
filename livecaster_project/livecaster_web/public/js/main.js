f1855158d065fd5dda4e8a18f706e460377534cb
Ehtsham Fareed <57910737+ehtshamfareed@users.noreply.github.com>
2020-06-28 22:48:03 +0500

    updated
    updated

--- livecaster_project/livecaster_web/public/js/main.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-	$('a.confirmDeletion').on('click', function () {
-		if(!confirm('Confirm deletion')){
-			console.log("This is confirmDeletion");
-			return false;
-		}
-	});
-	
-});
